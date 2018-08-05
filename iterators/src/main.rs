14dbb24a48a4d3825f983f352b4534e21d849f16
OsmanthusTree <MyOsmanthusTree@gmail.com>
2018-08-05 11:41:50 +0800

    iterators

---
+++ iterators/src/main.rs
@@ -0,0 +1,27 @@
+struct Counter {
+    count: u32,
+}
+impl Counter {
+    pub fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+fn main() {
+    let v: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    println!("{:?}", v);
+}
