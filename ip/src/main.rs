c1a17d98170fa6768f4c53fca10d02753a40e90d
OsmanthusTree <MyOsmanthusTree@gmail.com>
2018-08-28 14:17:57 +0800

    updated

--- ip/src/main.rs
+++ ip/src/main.rs
@@ -1,5 +1,19 @@
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::net::{TcpListener, TcpStream};
 fn main() {
-    let localhost_v4 = IpAddr::from(Ipv4Addr::new(127, 0, 0, 1));
-    println!("{:?}", localhost_v4.is_multicast());
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+
+    let mut buf = BufReader::new(stream);
+    buf.get_mut().write_all(b"hello world\r\n").unwrap();
+    buf.get_mut().flush().unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = BufReader::new(stream.unwrap());
+        let mut msg = String::new();
+        stream.read_line(&mut msg).unwrap();
+        msg = msg.trim().to_string();
+        println!("{}", msg);
+    }
 }
--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,27 +1,8 @@
-struct Counter {
-    count: u32,
-}
-impl Counter {
-    pub fn new() -> Counter {
-        Counter { count: 0 }
-    }
-}
-impl Iterator for Counter {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.count += 1;
-        if self.count < 6 {
-            Some(self.count)
-        } else {
-            None
-        }
-    }
-}
 fn main() {
-    let v: u32 = Counter::new()
-        .zip(Counter::new().skip(1))
-        .map(|(a, b)| a * b)
-        .filter(|x| x % 3 == 0)
-        .sum();
-    println!("{:?}", v);
+    let s = "how many roads must a man walk down\n"
+        .trim()
+        .split_whitespace();
+    for w in s {
+        println!("{}", w);
+    }
 }
