97718a9c469ddea3425730c30d0eacb4c15104fe
iosmanthus <MyOsmanthusTree@gmail.com>
2018-09-16 16:41:00 +0800

    unsafe codes

--- unsafe-block/src/main.rs
+++ unsafe-block/src/main.rs
@@ -1,3 +1,26 @@
+trait Messager {
+    fn send(&self) {
+        println!("Hello!");
+    }
+}
+
+#[allow(dead_code)]
+struct Window<'a> {
+    notifier: Box<&'a Messager>,
+    monitor: &'a str,
+}
+
 fn main() {
-    println!("Hello, world!");
+    struct MessageBox {};
+    impl Messager for MessageBox {}
+
+    let msg_box = MessageBox {};
+    let monitor = "aoc";
+
+    let window = Window {
+        notifier: Box::new(&msg_box),
+        monitor,
+    };
+
+    window.notifier.send();
 }
