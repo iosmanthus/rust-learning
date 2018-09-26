9dbbbf8753d104589d402d94f64a28ed0d4a0ef9
iosmanthus <MyOsmanthusTree@gmail.com>
2018-09-26 21:43:56 +0800

    add short function to compare the length of string slices

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,7 +1,11 @@
-fn shorter(s1:) {
-
+fn shorter<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() < s2.len() {
+        s1
+    } else {
+        s2
+    }
 }
 
 fn main() {
-
+    println!("{}", shorter("hello", "world?"));
 }
