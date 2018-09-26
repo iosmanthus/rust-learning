b502f66b118f3e061a2a12b7025f0517540afc7c
iosmanthus <MyOsmanthusTree@gmail.com>
2018-09-26 21:25:59 +0800

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,10 @@
+type Multiplier = Fn(i32) -> i32;
+
+fn multiplier(c: i32) -> Box<Multiplier> {
+    Box::new(move |x| c * x)
+}
+
+fn main() {
+    let m = multiplier(3);
+    println!("{}", m(4));
+}
