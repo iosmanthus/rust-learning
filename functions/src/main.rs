3caf76f812df0d550c122009930725bc333d8b38
iosmanthus <myosmanthustree@gmail.com>
2018-09-29 23:21:46 +0800

    impl trait

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,10 @@
-type Multiplier = Fn(i32) -> i32;
+//type Multiplier = Fn(i32) -> i32;
 
-fn multiplier(c: i32) -> Box<Multiplier> {
+/*fn multiplier(c: i32) -> Box<Multiplier> {
     Box::new(move |x| c * x)
+}*/
+fn multiplier(c: i32) -> impl FnOnce(i32) -> i32 {
+    move |x| c * x
 }
 
 fn main() {
